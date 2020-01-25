71de28b0b80fdd4340d5ac570043bba64344cba3
Md Mojahidul Islam <shahin.kuet@gmail.com>
2020-01-25 17:41:53 -0500

    LeetCode Count and Say solution. Accepted.

---
+++ src/come/leetkode/easy/CountAndSay.java
@@ -0,0 +1,42 @@
+package come.leetkode.easy;
+
+/*
+ * LeetCode Count and Say Problem
+ * Difficulty - Easy
+ * 01/25/2020
+ */
+public class CountAndSay {
+
+	public static String countAndSay(int n) {
+
+		String result = "1";
+
+		for (int i = 1; i < n; i++) {
+			result = findToSay(result);
+		}
+
+		return result;
+	}
+
+	public static String findToSay(String input) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < input.length();) {
+			int j = 0;
+
+			while (i + j < input.length() && input.charAt(i) == input.charAt(i + j)) {
+				j++;
+			}
+			sb.append(j).append(input.charAt(i));
+
+			i += j;
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("Input number = 8, output = " + countAndSay(8));
+
+	}
+
+}
